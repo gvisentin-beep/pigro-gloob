8f4ebd04598f3233a164d388188b6fae16e79205
gvisentin-beep <g.visentin@yahoo.it>
2026-04-30 12:03:15 +0200

    Remove aiCoreSeries and comboSeries calculations
    Removed aiCoreSeries and comboSeries calculations from app.js.

--- static/app.js
+++ static/app.js
@@ -1331,18 +1331,7 @@ function renderMain(labels, firstVals, secondVals, secondLabel, aiCoreSeries, co
       if (!labels.length) throw new Error(tr("noData"));
 
       const pigroSeries = rebalancePortfolio(labels, aligned.ls80, aligned.gold, aligned.btc, capital);
-      
-const aiCoreSeries = removeIsolatedSpikes(
-  benchmarkSeries(aligned, "world", capital),
-  0.10
-);
-
-const comboSeries = removeIsolatedSpikes(
-  pigroSeries.map((v, i) => 0.6 * v + 0.4 * aiCoreSeries[i]),
-  0.10
-);
-
-      
+            
             let secondSeries = [];
       let secondLabel = getBenchmarkLabel(currentBenchmark);
 
