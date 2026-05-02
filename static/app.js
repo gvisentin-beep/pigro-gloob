1826f6affe3a7574a387f241395c9fd8f423a48f
gvisentin-beep <g.visentin@yahoo.it>
2026-05-02 11:23:04 +0200

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to the app.

--- static/app.js
+++ static/app.js
@@ -1,3 +1,18 @@
+// Google Analytics
+(function(){
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-DSVT0D8ZX4";
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+
+  gtag('js', new Date());
+  gtag('config', 'G-DSVT0D8ZX4');
+})();
+
 (function () {
   let mainChart = null;
   let ddChart = null;
